87cc07595c482224b7d217d369d1eb7a3e16485b
Marcin Marciszewski <marcin@Sunnyvale.local>
2020-07-24 17:55:57 +0100

    start lecture 28

--- wp-content/themes/udemy/single.php
+++ wp-content/themes/udemy/single.php
@@ -132,9 +132,9 @@
                             <?php
                                 $categories = get_the_category();
                                 $rp_query = new WP_Query([
-                                    'post_per_page' => 2,
+                                    'posts_per_page' => 2,
                                     'post__not_in' => [$post->ID],
-                                    'cat'=> !empty($categories) ? $categories[0]->term_id:null
+                                    'cat'=> !empty($categories) ? $categories[0]->term_id : null,
                                 ]);
 
                                 if($rp_query->have_posts()){
@@ -158,44 +158,26 @@
                                             <div class="entry-c">
                                             <div class="entry-title">
                                                 <h4>
-                                                <a href="#">
-                                                    This is an Image Post
+                                                <a href="<?php the_permalink(); ?>">
+                                                <?php the_title(); ?>
                                                 </a>
                                                 </h4>
                                             </div>
                                             <ul class="entry-meta clearfix">
-                                                <li><i class="icon-calendar3"></i> 10th July 2014</li>
-                                                <li><i class="icon-comments"></i> 12</li>
+                                                <li><i class="icon-calendar3"></i> <?php echo get_the_date(); ?></li>
+                                                <li><i class="icon-comments"></i> <?php comments_number('0');?></li>
                                             </ul>
                                             <div class="entry-content">
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing
-                                                elit. Mollitia nisi perferendis.
+                                                <?php the_excerpt();?>
                                             </div>
                                             </div>
                                         </div>
                                         <?php
                                     }
+
+                                    wp_reset_postdata();
                                 }
                             ?>
-
-
-                        <div class="mpost clearfix">
-                            <div class="entry-image">
-                            <a href="#"><img src="images/blog/small/20.jpg" alt="Blog Single"></a>
-                            </div>
-                            <div class="entry-c">
-                            <div class="entry-title">
-                                <h4><a href="#">This is a Video Post</a></h4>
-                            </div>
-                            <ul class="entry-meta clearfix">
-                                <li><i class="icon-calendar3"></i> 24th July 2014</li>
-                                <li><i class="icon-comments"></i> 16</li>
-                            </ul>
-                            <div class="entry-content">Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Mollitia nisi perferendis.</div>
-                            </div>
-                        </div>
-
                         </div>
                         <?php 
                         if(comments_open() || get_comments_number()){
